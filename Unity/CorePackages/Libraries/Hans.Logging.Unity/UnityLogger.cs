46a2e0a31118149dfdcf6f453d099db83d3a8498
jthans <jthans@users.noreply.github.com>
2018-12-05 22:09:59 -0500

    Adding Unity Logger
    Adding the Unity Logger, to output to the Unity console, and the solution file required for the Unity project.

---
+++ Unity/CorePackages/Libraries/Hans.Logging.Unity/UnityLogger.cs
@@ -0,0 +1,37 @@
+﻿using Hans.Logging.Interfaces;
+using System.ComponentModel.Composition;
+using Hans.Logging.Models;
+using UnityEngine;
+
+namespace Hans.Logging.Unity
+{
+    /// <summary>
+    ///  Log Exporter that will export log messages out to the Unity engine, to display in the Console/any other aggregators that read from Unity's log feed.
+    /// </summary>
+    [Export(typeof(ILogExporter))]
+    public class UnityLogger : ILogExporter
+    {
+        /// <summary>
+        ///  Exports the log message to the Unity console.
+        /// </summary>
+        /// <param name="logToExport">The log message to send out to the console.</param>
+        public void ExportLog(Log logToExport)
+        {
+            switch (logToExport.Level)
+            {
+                case Enums.LogLevel.Warning:
+                    Debug.LogWarning(logToExport.ToString());
+                    break;
+                case Enums.LogLevel.Error:
+                case Enums.LogLevel.Fatal:
+                    Debug.LogError(logToExport.ToString());
+                    break;
+                case Enums.LogLevel.Debug:
+                case Enums.LogLevel.Information:
+                default:
+                    Debug.Log(logToExport.ToString());
+                    break;
+            }
+        }
+    }
+}
