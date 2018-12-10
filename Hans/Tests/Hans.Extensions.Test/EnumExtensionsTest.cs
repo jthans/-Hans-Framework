19c2d840f8415cef68d7946122e4c6b340efc759
jthans <jthans@users.noreply.github.com>
2018-12-09 20:27:42 -0500

    Adding EnumExtensions.
    Hans.Extensions (v1.0.6)
    Hans.Extensions.Test (v1.0.5)
    
    Adding IsFlagSet, to determine if certain flags are set in an enum.

---
+++ Hans/Tests/Hans.Extensions.Test/EnumExtensionsTest.cs
@@ -0,0 +1,54 @@
+﻿using Hans.Extensions;
+using Hans.Extensions.Test.Enums;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Hans.Extensions.Test
+{
+    /// <summary>
+    ///  Tests any extensions residing in this library for enumerations.
+    /// </summary>
+    [TestClass]
+    public class EnumExtensionsTest
+    {
+        #region IsFlagSet
+
+        /// <summary>
+        ///  Ensures that when checking for existing flags in an enum, it returns true successfully.
+        /// </summary>
+        [TestMethod]
+        public void IsFlagSet_ContainsFlagSuccessfully()
+        {
+            FlagEnumTest testEnum = (FlagEnumTest.FlagThree | FlagEnumTest.FlagTwo);
+
+            // Asserts
+            Assert.IsTrue(testEnum.IsFlagSet(FlagEnumTest.FlagTwo));
+            Assert.IsTrue(testEnum.IsFlagSet(FlagEnumTest.FlagThree));
+            Assert.IsTrue(testEnum.IsFlagSet((FlagEnumTest.FlagTwo | FlagEnumTest.FlagThree)));
+        }
+
+        /// <summary>
+        ///  Ensures that when the flag doesn't exist in an enum, it returns false successfully.
+        /// </summary>
+        [TestMethod]
+        public void IsFlagSet_DoesntContainFlagSuccessfully()
+        {
+            FlagEnumTest testEnum = (FlagEnumTest.FlagThree | FlagEnumTest.FlagTwo);
+
+            Assert.IsFalse(testEnum.IsFlagSet(FlagEnumTest.FlagOne));
+            Assert.IsFalse(testEnum.IsFlagSet(FlagEnumTest.FlagFour));
+        }
+
+        /// <summary>
+        ///  Ensures that when an enum is calculated with a value of 0, we throw the correct exception.
+        /// </summary>
+        [TestMethod]
+        public void IsFlagSet_ThrowsExceptionOnFlagValueZero()
+        {
+            FlagEnumTest testEnum = (FlagEnumTest.FlagThree | FlagEnumTest.FlagTwo);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => testEnum.IsFlagSet(FlagEnumTest.Invalid));
+        }
+
+        #endregion
+    }
+}
---
+++ Hans/Tests/Hans.Extensions.Test/Enums/FlagEnumTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Hans.Extensions.Test.Enums
+{
+    /// <summary>
+    ///  Enum Test that Contains Flags.
+    /// </summary>
+    [Flags]
+    public enum FlagEnumTest
+    {
+        Invalid = 0,
+
+        FlagOne = 1,
+
+        FlagTwo = 2,
+
+        FlagThree = 4,
+
+        FlagFour = 8
+    }
+}
