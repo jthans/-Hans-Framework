e4e4ece298d48051f37eebaaf9d465ce2679132d
Taylor Hans <taylor.hans7@gmail.com>
2020-03-31 17:44:05 -0400

    JSON/Dialogue Converted

---
+++ Hans/Hans.DialogueEngine.Standard/Entities/ConversationNode.cs
@@ -0,0 +1,66 @@
+﻿using Hans.JSON;
+using System;
+using System.Collections.Generic;
+
+namespace Hans.DialogueEngine.Entities
+{
+    /// <summary>
+    ///  Node in a conversation, that represents a single thing said by a character, or
+    ///     particpant in the conversation.
+    /// </summary>
+    public class ConversationNode : JSONEntity
+    {
+        #region Properties
+
+        /// <summary>
+        ///  The ID of the node, so that they can be accessed form other nodes.
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        ///  If this GUID is the initial node of the conversational tree.
+        /// </summary>
+        public bool Initial { get; set; }
+
+        /// <summary>
+        ///  The dialogue of this node, either said as a prompt, answer, etc.
+        /// </summary>
+        public string Dialogue { get; set; }
+
+        /// <summary>
+        ///  The list of actions that will be executed upon activation of this node.s
+        /// </summary>
+        public List<DialogueCheck> DialogueActions { get; set; }
+
+        /// <summary>
+        ///  Indicates if this node is enabled or not, null indicates a calculation has not been run.
+        /// </summary>
+        public bool? IsEnabled { get; private set; }
+
+        /// <summary>
+        ///  The list of nodes that are next in the sequence.
+        /// </summary>
+        public List<NextNodeModel> NextNodes { get; set; }
+
+        /// <summary>
+        ///  The checks required for this dialogue to be allowed to execute.
+        /// </summary>
+        public List<DialogueCheck> RequiredChecks { get; set; }
+
+        #endregion
+
+        #region Instance Methods
+
+        /// <summary>
+        ///  Sets the isEnabled flag to indicate whether or not this conversation node has been enabled.  This is 
+        ///     set when a calculation has already been run, so we're not running it every time.
+        /// </summary>
+        /// <param name="isEnabled"></param>
+        public void SetEnabled(bool isEnabled = true)
+        {
+            this.IsEnabled = isEnabled;
+        }
+
+        #endregion
+    }
+}
---
+++ Hans/Hans.DialogueEngine.Standard/Interfaces/IConversationComponentMetadata.cs
@@ -0,0 +1,15 @@
+﻿namespace Hans.DialogueEngine.Interfaces
+{
+    /// <summary>
+    ///  Metadata class that holds information about a conversational method that needs to
+    ///     be inherited in custom situations to move forward the conversation.
+    /// </summary>
+    public interface IConversationComponentMetadata
+    {
+        /// <summary>
+        ///  Description of this component.  Is going to tie into any GUI that will be used in
+        ///     creating these conversations, and is how we locate the component to run.
+        /// </summary>
+        string Description { get; }
+    }
+}
