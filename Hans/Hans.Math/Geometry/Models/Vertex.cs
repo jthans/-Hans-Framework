39b34a34e078898d604dd14ffd87305279068bf3
Taylor Hans <taylor.hans7@gmail.com>
2020-03-31 18:28:44 -0400

    Fixing Math Refs

---
+++ Hans/Hans.Math/Geometry/Models/Vertex.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Hans.Math.Geometry.Models
+{
+    /// <summary>
+    ///  Represents a Vertex in 2D Space - X/Y Coords, along with other supporting information.
+    /// </summary>
+    [Serializable]
+    public sealed class Vertex : Point
+    {
+        /// <summary>
+        ///  The index of this vertex, to identify it within a gridspace or polygon.
+        /// </summary>
+        public int Index { get; set; }
+
+        #region Constructors
+
+        /// <summary>
+        ///  Initializes a new instance of the <see cref="Vertex" /> class.
+        /// </summary>
+        public Vertex()
+        {
+
+        }
+
+        /// <summary>
+        ///  Initializes a new instance of the <see cref="Vertex" /> class, with coordinate information only.
+        /// </summary>
+        /// <param name="x">X Coord</param>
+        /// <param name="y">Y Coord</param>
+        public Vertex (double x, double y)
+            : base (x, y)
+        {
+
+        }
+
+        /// <summary>
+        ///  Initializes a new instance of the <see cref="Vertex" /> class.  
+        /// </summary>
+        /// <param name="index">Index of a given vertex, in context of the other vertices.</param>
+        /// <param name="x">X Coord</param>
+        /// <param name="y">Y Coord</param>
+        public Vertex(int index, double x, double y)
+            : base(x, y)
+        {
+            this.Index = index;
+        }
+
+        #endregion
+    }
+}
---
+++ Hans/Hans.Math/Geometry/TriangleHelper.cs
@@ -0,0 +1,33 @@
+﻿namespace Hans.Math.Geometry
+{
+    /// <summary>
+    ///  Helper class that focuses on triangle and angle-based calculations.  This involves a lot of
+    ///     trigonometric functionality.
+    /// </summary>
+    public static class TriangleHelper
+    {
+        /// <summary>
+        ///  Calculates a given angle in a triangle provided 3 sides, using the Cosine theorum.
+        /// </summary>
+        /// <param name="a">Side A Length</param>
+        /// <param name="b">Side B Length</param>
+        /// <param name="c">Side C Length</param>
+        /// <returns>The angle across from side C, in degrees.</returns>
+        public static double CalculateAngleFromTriangleSides(double a, double b, double c, int? numDigits = null)
+        {
+            var calcResult = System.Math.Acos((
+                                System.Math.Pow(a, 2) +
+                                System.Math.Pow(b, 2) -
+                                System.Math.Pow(c, 2)) /
+                                (2.0 * a * b)) *
+                                (180.0 / System.Math.PI); // Conversion into degrees.
+
+            if (numDigits.HasValue)
+            {
+                calcResult = System.Math.Round(calcResult, numDigits.Value);
+            }
+
+            return calcResult;
+        }
+    }
+}
